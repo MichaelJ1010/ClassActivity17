b0ff5c82e3a94f30e3caca12c5de445647577827
Michael J <30031129@cc.peralta.edu>
2024-10-07 10:07:06 -0700

    printed if target was found

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ using namespace std;
 int linearSearch(int list[], int size, int target){
     for(int i = 0; i < size; i++){
         if(list[i] == target){
+            cout << "target at index " <<i << endl;
             return i;
         }
     }
+    cout << "didn't find target" << endl;
     return -1;
 }
 int main(){
-
+    int integerArray[] = {1,2,3};
+    linearSearch(integerArray, 3, 2);
 }
